170b6872792bc5d1bab3d241826e4873b1f4a878
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-11-26 13:25:04 +0100

    updated signature with correct types

--- src/plotly-stubs/colors/_funcs.pyi
+++ src/plotly-stubs/colors/_funcs.pyi
@@ -1,9 +1,12 @@
 from collections.abc import Iterable, Sequence
+from typing import Literal, TypeAlias
+
+ColorType: TypeAlias = Literal["rgb", "tuple"]
 
 def sample_colorscale(
     colorscale: Sequence[str] | str,
     samplepoints: Iterable[float] | int,
     low: float = 0.0,
     high: float = 1.0,
-    colortype: str = "rgb",
-) -> list[object]: ...
+    colortype: ColorType = "rgb",
+) -> list[str] | list[tuple[float, float, float]]: ...
